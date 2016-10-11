c8ee86fbde8601e68347fc62115cb3a7745c97c8
biks152207 <bikrambasnet1@gmail.com>
2016-10-11 09:06:05 +0545

    first commit

--- server.js
+++ server.js
@@ -5,25 +5,23 @@ const mongojs = require('mongojs');
 
 // Create a server with a host and port
 const server = new Hapi.Server();
+
+
 server.connection({
-    port: 3000
+  port: 3000
 });
 
-//Connect to db
+// Connect to db
 server.app.db = mongojs('hapi-rest-mongo', ['books']);
 
-//Load plugins and start server
+// Load plugins and start server
 server.register([
-    require('./routes/books')
-], (err) => {
-
-    if (err) {
-        throw err;
-    }
-
-    // Start the server
-    server.start((err) => {
-        console.log('Server running at:', server.info.uri);
-    });
-
-});
+  require('./routes/books')
+], (err) =>{
+  if (err){
+    throw err;
+  }
+  server.start((err) =>{
+    console.log('Server running at: ', server.info.uri);
+  })
+})
